d6c243f6ad8a437d8db7ade98786a885042405b0
pugazhenthik <pugazhwoo@gmail.com>
2024-08-16 09:55:50 +0530

    App routes created

--- app.js
+++ app.js
@@ -5,23 +5,14 @@ const {
     errorHandler,
 } = require('./src/middlewares/errorMiddleware');
 
+const appRouter = require('./src/routes/appRoutes');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    const posts = [
-        {
-            id: 1,
-            title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus reprehenderit',
-            body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus, nihil consequuntur? Praesentium minima cumque similique porro dolorem neque, aut natus incidunt! Autem inventore minus voluptatem reprehenderit non dicta placeat recusandae?',
-            createdAt: new Date().getDay(),
-            isActive: true,
-        },
-    ];
-    res.json({ data: posts });
-});
+app.use(appRouter);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
