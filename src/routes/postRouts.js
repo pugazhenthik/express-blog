31bdf73dc723744b752867f8d2e018986369f745
pugazhenthik <pugazhwoo@gmail.com>
2024-08-17 23:15:59 +0530

    Post routes updated

--- src/routes/postRouts.js
+++ src/routes/postRouts.js
@@ -1,37 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const PostController = require('../controllers/postController');
 
-router.get('/', (req, res) => {
-    const posts = [
-        {
-            id: 1,
-            title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus reprehenderit',
-            body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus, nihil consequuntur? Praesentium minima cumque similique porro dolorem neque, aut natus incidunt! Autem inventore minus voluptatem reprehenderit non dicta placeat recusandae?',
-            createdAt: new Date().getDay(),
-            isActive: true,
-        },
-        {
-            id: 1,
-            title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus reprehenderit',
-            body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus, nihil consequuntur? Praesentium minima cumque similique porro dolorem neque, aut natus incidunt! Autem inventore minus voluptatem reprehenderit non dicta placeat recusandae?',
-            createdAt: new Date().getDay(),
-            isActive: true,
-        },
-    ];
-
-    res.json({ data: posts });
-});
-
-router.get('/:id', (req, res) => {
-    const post = {
-        id: 1,
-        title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus reprehenderit',
-        body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus, nihil consequuntur? Praesentium minima cumque similique porro dolorem neque, aut natus incidunt! Autem inventore minus voluptatem reprehenderit non dicta placeat recusandae?',
-        createdAt: new Date().getDay(),
-        isActive: true,
-    };
-
-    res.json({ data: post });
-});
+router.get('/', PostController.getPosts);
+router.get('/:id', PostController.getPost);
+router.post('/', PostController.createPost);
+router.put('/:id', PostController.updatePost);
+router.delete('/:id', PostController.deletePost);
 
 module.exports = router;
