e966283b9643b0a9e8cd74a2b2c96f6ce2941a76
pugazhenthik <pugazhwoo@gmail.com>
2024-08-17 23:11:33 +0530

    Post controller and model created

---
+++ src/controllers/postController.js
@@ -0,0 +1,79 @@
+const Post = require('../models/Post');
+
+const getPosts = async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Something went wrong',
+            error: error.message,
+        });
+    }
+};
+
+const getPost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Something went wrong',
+            error: error.message,
+        });
+    }
+};
+
+const createPost = async (req, res) => {
+    try {
+        const post = new Post(req.body);
+        await post.save();
+        res.status(201).json(post);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error creating post',
+            error: error.message,
+        });
+    }
+};
+
+const updatePost = async (req, res) => {
+    try {
+        const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
+        if (!post) {
+            return res.status(404).json({ message: 'Post not found' });
+        }
+        res.json(post);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error updating post',
+            error: error.message,
+        });
+    }
+
+    const deletePost = async (req, res) => {
+        try {
+            const post = await Post.findByIdAndDelete(req.params.id);
+            if (!post) {
+                res.status(500).json({ message: 'Post not deleted' });
+            }
+
+            res.status(200).json({ message: 'Post deleted successfully' });
+        } catch (error) {
+            res.status(500).json({
+                message: 'Something went wrong',
+                error: error.message,
+            });
+        }
+    };
+};
+
+module.exports = {
+    getPosts,
+    getPost,
+    createPost,
+    updatePost,
+    deletePost,
+};
