b28f76d6727800e92c583b3bf32af8fdf915f5f6
pugazhenthik <pugazhwoo@gmail.com>
2024-08-25 18:59:47 +0530

    Category model updated

--- src/models/Category.js
+++ src/models/Category.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
     name: {
         type: String,
-        require: true,
+        required: true,
         min: 3,
     },
     description: {
