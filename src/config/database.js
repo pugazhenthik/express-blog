326bd172740afaf2f4b6a9454cfc26d47719a683
pugazhenthik <pugazhwoo@gmail.com>
2024-08-25 20:47:51 +0530

    Database test coverage added

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,4 @@
 const mongoose = require('mongoose');
-
-// const password = process.env.DB_PASSWORD;
-// const username = process.env.DB_USERNAME;
-// const hostname = process.env.DB_HOSTNAME;
-// const appname = process.env.DB_APPNAME;
-
-// const url = `mongodb+srv://${username}:${password}@${hostname}.mongodb.net/blog?retryWrites=true&w=majority&appName=${appname}`;
 const url =
     'mongodb://127.0.0.1:27017/express_blog?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.0';
 
@@ -15,14 +8,17 @@ const clientOptions = {
 
 const connectDB = async () => {
     try {
-        await mongoose.disconnect();
-        await mongoose.connect(url, clientOptions);
-
-        await mongoose.connection.db.admin().command({ ping: 1 });
-        console.log(`MongoDB Connected`);
+        const connection = await mongoose.connect(url, clientOptions);
+        if (connection) {
+            await mongoose.connection.db.admin().command({ ping: 1 });
+            console.log(`MongoDB Connected`);
+        } else {
+            console.log('MongoDB connection failed');
+        }
     } catch (error) {
         console.log(`MongoDB Not Connected`);
+        console.log(error.message);
     }
 };
 
-module.exports = connectDB;
+module.exports = { connectDB };
