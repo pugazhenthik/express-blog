7b57463e609a32b3f4c4bd7be72bbaf7e85de111
pugazhenthik <pugazhwoo@gmail.com>
2024-09-11 21:49:09 +0530

    Auth middleware added to tags

--- src/routes/tagRoutes.js
+++ src/routes/tagRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const TagController = require('../controllers/tagController');
+const auth = require('../middlewares/authMiddleware');
 
-router.get('/', TagController.getTags);
-router.get('/:id', TagController.getTag);
-router.post('/', TagController.createTag);
-router.put('/:id', TagController.updateTag);
-router.delete('/:id', TagController.deleteTag);
+router.get('/', auth, TagController.getTags);
+router.get('/:id', auth, TagController.getTag);
+router.post('/', auth, TagController.createTag);
+router.put('/:id', auth, TagController.updateTag);
+router.delete('/:id', auth, TagController.deleteTag);
 
 module.exports = router;
