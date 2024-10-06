92261d6d0bafd4e45d8544459e9a7b7eb9576153
pugazhenthik <pugazhwoo@gmail.com>
2024-10-06 18:21:32 +0530

    Auth login issue fixed

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,7 +6,7 @@ const { randomBytes } = require('crypto');
 const login = async (req, res) => {
     try {
         const { email, password } = req.body;
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ email }).select('+password');
         if (!user) {
             return res
                 .status(401)
